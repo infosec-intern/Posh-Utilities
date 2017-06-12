f0353af6c7e308760151af6595d2be77f72b2ca6
Thomas Gardner <tpgardner19@gmail.com>
2017-06-11 21:44:41 -0500

    added get-help message and expanded parameters

--- Get-ScriptBlock.ps1
+++ Get-ScriptBlock.ps1
@@ -1,19 +1,39 @@
 <#
-    Grab all
+.SYNOPSIS
+
+.DESCRIPTION
+.PARAMETER ComputerName
+    Remote computer to search event logs on. Default is the local machine
+.PARAMETER Credential
+    PSCredential object to authenticate to a remote computer with
+.PARAMETER List
+    Grab the paths of all scripts that have been run and the last time they were run
+.PARAMETER Dump
+    Save the contents of all scripts to a designated folder
+.PARAMETER ScriptName
+    Save the contents of a particular script to a designated folder
+.PARAMETER OutFolder
+    Particular folder to save scripts to. Default is the current one
+.EXAMPLE
 #>
 [CmdletBinding(DefaultParameterSetName="List")]
 
 Param(
-    [Parameter(ParameterSetName="List")]
-    [switch]$List,
     [Parameter()]
     [string]$ComputerName = ".",
     [Parameter()]
     [PSCredential]$Credential,
+    [Parameter(ParameterSetName="List")]
+    [switch]$List,
+    [Parameter(ParameterSetName="Dump")]
+    [switch]$Dump,
     [Parameter(ParameterSetName="Script", ValueFromPipeline=$true)]
+    [ValidateNotNullOrEmpty()]
     [string]$ScriptName,
+    [Parameter(ParameterSetName="Dump")]
     [Parameter(ParameterSetName="Script")]
-    [string]$OutFolder = "$env:USERPROFILE\Downloads"
+    [ValidateNotNullOrEmpty()]
+    [string]$OutFolder = "$env:USERPROFILE\Desktop"
 )
 
 $Events = Get-WinEvent -MaxEvents 1 -FilterHashtable @{
@@ -33,8 +53,7 @@ $Events | ForEach-Object {
     $ScriptPath = $EventXML.Event.EventData.Data[4].'#text'
 
     # If "Script" ParameterSetName and a script to search for are set, then only write out that script if it exists
-    If ($PsCmdlet.ParameterSetName -eq "Script" -and $ScriptName) {
-        If ($ScriptName -ne )
+    If ($PsCmdlet.ParameterSetName -eq "Script") {
     }
     # If ParameterSetName isn't "Script", then assume it's "List" and write out all scripts
     Else {
