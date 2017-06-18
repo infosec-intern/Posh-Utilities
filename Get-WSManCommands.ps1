7f6a612b5fe9c814ba0a00ec8c6e643408e8871e
Thomas Gardner <tpgardner19@gmail.com>
2017-06-18 10:51:12 -0500

    added parameters, an example, and a link

--- Get-WSManCommands.ps1
+++ Get-WSManCommands.ps1
@@ -5,6 +5,16 @@
     Analyze event logs for information about any PSSession remote management sessions (also known as WSMan).
     This includes when sessions are started and stopped, extracting any hex-encoded commands run during the sessions,
     and reporting any errors that occurred during the sessions
+.PARAMETER ComputerName
+    Remote computer to search event logs on. Default is the local computer
+.PARAMETER Credential
+    PSCredential object to authenticate to a remote computer with
+.PARAMETER List
+    List the WSMan sessions and their start/stop times
+.EXAMPLE
+    .\Get-WSManCommands.ps1
+.LINK
+    https://github.com/infosec-intern/Posh-Utilities/
 #>
 [CmdletBinding()]
 
