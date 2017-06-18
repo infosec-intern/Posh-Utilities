84db950366ba4927621332703694fd6c5cf6330c
Thomas Gardner <tpgardner19@gmail.com>
2017-06-18 10:45:43 -0500

    updated help

--- Get-WSManCommands.ps1
+++ Get-WSManCommands.ps1
@@ -1,6 +1,10 @@
 <#
-    Get the hex-encoded commands run during a PSSession
-    remote management session (also known as WSMan)
+.SYNOPSIS
+    Analyze event logs for information about any PSSession remote management sessions (also known as WSMan)
+.DESCRIPTION
+    Analyze event logs for information about any PSSession remote management sessions (also known as WSMan).
+    This includes when sessions are started and stopped, extracting any hex-encoded commands run during the sessions,
+    and reporting any errors that occurred during the sessions
 #>
 [CmdletBinding()]
 
