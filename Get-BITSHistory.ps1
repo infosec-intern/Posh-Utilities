66805d15f34c548977560f48aa83268fe0d92e57
Thomas Gardner <tpgardner19@gmail.com>
2017-06-18 13:42:51 -0500

    added some notes about bits transfer logs

--- Get-BITSHistory.ps1
+++ Get-BITSHistory.ps1
@@ -24,3 +24,22 @@ Param(
     [Parameter(ParameterSetName="List")]
     [switch]$List
 )
+
+# Event ID 3 shows start times and session ID
+#   The BITS service created a new job.
+#   Transfer job: Push Notification Platform Job: 1
+#   Job ID: {b652f1e5-5a4f-4a3c-9673-21b527e3b142}
+#   Owner: computer\Username
+#   Process Path: C:\Windows\System32\svchost.exe
+#   Process ID: 3388
+# Event ID 4 shows stop times and session ID
+#   The transfer job is complete.
+#   User: computer\Username
+#   Transfer job: Push Notification Platform Job: 1
+#   Job ID: {b0bd0c0e-7724-49e2-a627-7b9faaa32316}
+#   Owner: computer\Username
+#   File count: 1
+# Event ID 5 shows cancelled jobs
+#   Job cancelled. User: NT AUTHORITY\NETWORK SERVICE, job: MicrosoftMapsBingGeoStore, jobID: {8636eee8-dc04-472e-aa33-60838f7a4232}, owner: NT AUTHORITY\NETWORK SERVICE, filecount: 0
+# Event ID 59 shows URL for session
+#   BITS started the Push Notification Platform Job: 1 transfer job that is associated with the http://google.com/ URL.
