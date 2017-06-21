e1a71cf9126b5a8501ee0b2d296d2cfc41c14fe9
Thomas Gardner <tpgardner19@gmail.com>
2017-06-20 20:47:55 -0500

    added help for posh-zip

--- Posh-Zip.psm1
+++ Posh-Zip.psm1
@@ -1,60 +1,72 @@
-# Taken From:
-# http://blogs.technet.com/b/heyscriptingguy/archive/2015/03/09/use-powershell-to-create-zip-archive-of-folder.aspx
 Function Invoke-Zip {
-	[CmdletBinding()]
-	Param(
-		[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
-			[String]$Path,
-        	[Parameter(Mandatory=$false,Position=1)]
-            		[String]$Name,
-        	[Parameter(Mandatory=$false)]
-            		[Switch]$Force
-	)
-	Begin
-	{
-        	Write-Verbose "Invoke-Zip: BEGIN"
-        	Write-Verbose "`$Path = $Path"
-        	if (-not (Test-Path $Path -PathType Container))
-        	{
-            		Throw "$Path is not an existing directory"
-        	}
-        	Write-Verbose "Adding IO.Compression assembly"
-        	Add-Type -AssemblyName "System.IO.Compression.Filesystem"
-	}
-	Process
-	{
-        	Write-Verbose "Invoke-Zip: PROCESS"
-        	[IO.Compression.Zipfile]::CreateFromDirectory($Path, $Name)
-	}
-	End
-	{
-        	Write-Verbose "Invoke-Zip: END"
-	}
+<#
+.SYNOPSIS
+    Compress a folder or collection of files into a zip archive
+.PARAMETER Path
+    Folder to compress into a zip archive
+.PARAMETER Name
+    Name of resulting zip archive
+.EXAMPLE
+    Invoke-Zip -Path ~/Downloads -Name downloads.zip
+.LINK
+    http://blogs.technet.com/b/heyscriptingguy/archive/2015/03/09/use-powershell-to-create-zip-archive-of-folder.aspx
+#>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Path,
+        [Parameter(Mandatory=$false,Position=1)]
+        [String]$Name
+    )
+    Begin
+    {
+        if (-not (Test-Path $Path -PathType Container))
+        {
+            Throw "$Path is not an existing directory"
+        }
+        Add-Type -AssemblyName "System.IO.Compression.Filesystem"
+    }
+    Process
+    {
+        [IO.Compression.Zipfile]::CreateFromDirectory($Path, $Name)
+    }
+    End
+    {
+    }
 }
 
-# Taken From:
-# http://blogs.technet.com/b/heyscriptingguy/archive/2015/03/09/use-powershell-to-create-zip-archive-of-folder.aspx
 Function Invoke-Unzip {
-	[CmdletBinding()]
-	Param(
-		[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
-			[String]$Path,
-        	[Parameter(Mandatory=$true,Position=1)]
-            		[String]$Destination
-	)
-	Begin
-	{
-        	Write-Verbose "Invoke-Unzip: BEGIN"
-        	Add-Type -AssemblyName "System.IO.Compression.Filesystem"
-	}
-	Process
-	{
-        	Write-Verbose "Invoke-Unzip: PROCESS"
-	}
-	End
-	{
-        	Write-Verbose "Invoke-Unzip: END"
-	}
+<#
+.SYNOPSIS
+    Extract a zip archive into a folder or collection of files
+.PARAMETER Path
+    Archive to extract
+.PARAMETER Destination
+    Folder to extract data to
+.EXAMPLE
+    Invoke-Unzip -Path .\downloads.zip -Destination ~/Downloads
+.LINK
+    http://blogs.technet.com/b/heyscriptingguy/archive/2015/03/09/use-powershell-to-create-zip-archive-of-folder.aspx
+#>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Path,
+        [Parameter(Mandatory=$true,Position=1)]
+        [String]$Destination
+    )
+    Begin
+    {
+        Add-Type -AssemblyName "System.IO.Compression.Filesystem"
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
 }
 
 Export-ModuleMember Invoke-Zip
