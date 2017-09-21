f2d96727128c9fd94198d6c5cd862cb1255a4d4d
Thomas Gardner <tpgardner19@gmail.com>
2017-09-20 22:07:53 -0600

    created ETW-Explorations.ps1

---
+++ ETW-Explorations.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+Param()
+<#
+.SYNOPSIS
+    Some sample ETW explorations
+.DESCRIPTION
+    This module contains various functions and commands related to ETW (Event Tracing for Windows).
+    I really have no idea what's gonna be in it, so it probably won't be all that useful on its own.
+    Any future good ideas will get their own scripts/modules.
+.LINK
+    https://github.com/infosec-intern/Posh-Utilities/
+    https://support.microsoft.com/en-us/help/2593157/event-tracing-for-windows-etw-simplified
+    https://msdn.microsoft.com/en-us/library/windows/desktop/bb968803(v=vs.85).aspx
+#>
