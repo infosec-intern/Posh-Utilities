a7f910bb0d7b943f41d8eb7ac2a792c5f464d5b8
Thomas Gardner <tpgardner19@gmail.com>
2017-09-21 22:41:23 -0600

    added basic trace session doesn't appear to work

--- ETW-Explorations.ps1
+++ ETW-Explorations.ps1
@@ -13,5 +13,10 @@ Param()
     https://msdn.microsoft.com/en-us/library/windows/desktop/bb968803(v=vs.85).aspx
     https://www.fireeye.com/blog/threat-research/2017/09/pywintrace-python-wrapper-for-etw.html
     https://blogs.msdn.microsoft.com/ntdebugging/2009/09/08/part-2-exploring-and-decoding-etw-providers-using-event-log-channels/
+    https://technet.microsoft.com/en-us/library/dn168858.aspx
 #>
-
+$TraceSession = New-PefTraceSession -Mode Circular -SaveOnStop -Path "C:\Users\Thomas\Desktop\Trace.matu" -TotalSize 50
+$Trigger = New-PefTimeSpanTrigger -TimeSpan (New-TimeSpan -Seconds 10)
+Set-PefTraceFilter -PEFSession $TraceSession -Trigger $Trigger
+Add-PefMessageSource -PEFSession $TraceSession -Source "Microsoft-Pef-WFP-MessageProvider"
+Start-PefTraceSession -PEFSession $TraceSession
