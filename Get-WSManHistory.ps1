a0e76754bf4c382dbb81cfef5c52ca7e2633ed24
Thomas Gardner <tpgardner19@gmail.com>
2017-09-26 21:07:46 -0600

    modeling wsmanhistory after bitshistory

--- Get-WSManHistory.ps1
+++ Get-WSManHistory.ps1
@@ -9,31 +9,60 @@
     Remote computer to search event logs on. Default is the local computer
 .PARAMETER Credential
     PSCredential object to authenticate to a remote computer with
+.PARAMETER Path
+    Path to an event log (should end in .evtx) to read
 .PARAMETER List
     List the WSMan sessions and their start/stop times
 .EXAMPLE
-    .\Get-WSManCommands.ps1
+    .\Get-WSManHistory.ps1
 .LINK
     https://github.com/infosec-intern/Posh-Utilities/
+    Advanced Functions: https://technet.microsoft.com/en-us/library/hh413265.aspx
+.NOTES
+    Event ID
 #>
-[CmdletBinding(DefaultParameterSetName="List")]
+Function Get-WSManHistory {
+    [CmdletBinding(DefaultParameterSetName="List")]
+    Param(
+        [Parameter()]
+        [string]$ComputerName = "$env:COMPUTERNAME",
+        [Parameter()]
+        [PSCredential]$Credential,
+        [Parameter()]
+        [string]$Path,
+        [Parameter(ParameterSetName="List")]
+        [switch]$List
+    )
+    BEGIN {
+        # set up allthethings
+        $Commands = @{}
+        $Filter = @{
+            "ProviderName"="Microsoft-Windows-WinRM";
+            "LogName"="Microsoft-Windows-WinRM/Operational";
+        }
 
-Param(
-    [Parameter()]
-    [string]$ComputerName = "$env:COMPUTERNAME",
-    [Parameter()]
-    [PSCredential]$Credential,
-    [Parameter(ParameterSetName="List")]
-    [switch]$List
-)
+        If ($Path) {
+            $Filter.Add("Path", $Path)
+        }
 
-$EventLogFilter = @{
-    "ProviderName"="Microsoft-Windows-WinRM"
-}
-
-If ($Credential) {
-    $Events = Get-WinEvent -ComputerName $ComputerName -Credential $Credential -FilterHashtable $EventLogFilter
-}
-Else {
-    $Events = Get-WinEvent -ComputerName $ComputerName -FilterHashtable $EventLogFilter
+        If ($Credential) {
+            $Events = Get-WinEvent -Oldest -ComputerName $ComputerName -Credential $Credential -FilterHashtable $Filter
+        }
+        Else {
+            $Events = Get-WinEvent -Oldest -ComputerName $ComputerName -FilterHashtable $Filter
+        }
+    }
+    PROCESS {
+        ForEach ($Event in $Events) {
+            $Record = ([xml]$Event.ToXML()).Event.EventData.Data
+            Switch ($Event.Id) {
+                Default {
+                    Write-Warning -Message "I can't parse event ID $($Event.Id)"
+                }
+            }
+        }
+    }
+    END {
+        Write-Output -InputObject $Commands.Values
+    }
 }
